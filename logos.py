e9564ac53a8b0c64a7bdf47a0d6e405c1bdc9636
Matt Kenney <matt@nomonkey.net>
2024-02-28 19:09:10 -0500

    output JSON

--- logos.py
+++ logos.py
@@ -1,23 +1,89 @@
 #!/usr/bin/env python3
 
 import bz2
+import json
 import re
 import sys
 import urllib.parse
 import urllib.request
 import xml.sax
+from datetime import datetime, timezone
 from xml.sax.handler import ContentHandler
 
 # https://github.com/earwig/mwparserfromhell
 import mwparserfromhell as mwp
 
-lang = 'en'
 logoBase = 'https://en.wikipedia.org/wiki/Special:Redirect/file'
 url = 'http://localhost/enwiki-latest-pages-articles-multistream.xml.bz2'
+tags = frozenset(['logo'])
 
-articleCount = 0;
-infoCount = 0;
-svgLogoCount = 0;
+articleCount = 0
+infoCount = 0
+startTime = None
+svgLogoCount = 0
+
+class JsonHandler():
+  def __init__(self, file=sys.stdout):
+    self.count = 0
+    self.file = file
+
+  def add(self, img, name, src):
+    if self.count:
+      print(',', file=self.file)
+    else:
+      text = self.buildJson()
+      offset = text.find(']')
+      print(text[:offset], file=self.file)
+    print(json.dumps({'img': img, 'name': name, 'src': src }, separators=(',', ':')), end='', file=self.file)
+    self.count = self.count + 1
+
+  def buildJson(self):
+    return json.dumps({
+      'handle': '',
+      'images': [],
+      'lastmodified': isodatetime(),
+      'logo': '',
+      'name': '',
+      'provider': '',
+      'provider_icon': '',
+      'url': '',
+      'website': ''
+    }, indent=0)
+
+  def end(self):
+    print('', file=self.file)
+    text = self.buildJson()
+    offset = text.find(']')
+    print(text[offset:], file=self.file)
+
+class MarkupHandler():
+  def __init__(self, output):
+    self.output = output
+
+  def process(self, base, title, text):
+    global articleCount, infoCount, svgLogoCount
+    if articleCount % 1000 == 0:
+      now = datetime.utcnow()
+      delta = now - startTime
+      print(isodatetime(now), delta.total_seconds(), articleCount, infoCount, svgLogoCount, file=sys.stderr)
+    articleCount = articleCount + 1
+    parsed = mwp.parse(text)
+    hasInfobox = False
+    for template in parsed.ifilter_templates():
+      name = str(template.name).strip()
+      if name[0:7].lower() != 'infobox':
+        continue
+      infoCount = infoCount + 1
+      hasInfobox = True
+      for argument in template.params:
+        tag = str(argument.name).strip().lower()
+        if not tag in tags:
+          continue
+        pageUrl = urllib.parse.urljoin(base, urlencode(title))
+        logoUrls = imageValueToUrls(argument.value)
+        for logoUrl in logoUrls:
+          svgLogoCount = svgLogoCount + 1
+          self.output.add(logoUrl, title, pageUrl)
 
 class TextHandler(ContentHandler):
   def __init__(self, process):
@@ -32,7 +98,7 @@ class TextHandler(ContentHandler):
     self._chunks.append(chunk)
 
   def endElement(self, name):
-    text = ''.join(self._chunks);
+    text = ''.join(self._chunks)
     if name == 'text':
       self._process(self._base, self._title, text)
     elif name == 'base':
@@ -44,38 +110,10 @@ class TextHandler(ContentHandler):
     self._parser.feed(data)
 
   def startElement(self, name, attrs):
-    self._chunks = [];
-
-class MarkupHandler():
-  def process(self, base, title, text):
-    global articleCount, infoCount, svgLogoCount
-    if articleCount % 1000 == 0:
-      print(articleCount, infoCount, svgLogoCount, file=sys.stderr)
-    articleCount = articleCount + 1;
-    parsed = mwp.parse(text)
-    hasInfobox = False
-    for template in parsed.ifilter_templates():
-      name = str(template.name).strip()
-      if name[0:7].lower() != 'infobox':
-        continue
-      hasInfobox = True
-      for argument in template.params:
-        arg = str(argument.name).strip().lower()
-        if arg != 'logo':
-          continue
-        infoCount = infoCount + 1;
-        pageUrl = urllib.parse.urljoin(base, urlencode(title))
-        logoUrls = logoValueToUrls(argument.value)
-        for logoUrl in logoUrls:
-          svgLogoCount = svgLogoCount + 1
-          print('###################################################################')
-          print(pageUrl)
-          print(name)
-          print(logoUrl)
-          print('-------------------------------------------------------------------')
+    self._chunks = []
 
 def decompress(byteStream, feed):
-  bufsize = 128 * 1024;
+  bufsize = 128 * 1024
   decompressor = bz2.BZ2Decompressor()
   while True:
     compressed = byteStream.read(bufsize)
@@ -90,10 +128,26 @@ def decompress(byteStream, feed):
       else:
         break
 
+def isodatetime(when=None):
+  if when:
+    when = when.replace(tzinfo=timezone.utc)
+  else:
+    when = datetime.now(timezone.utc)
+  return when.isoformat()
+
+refile = re.compile(r'(?i)^ *\[\[ *file *:')
+def imageValueToUrls(value):
+  value = str(value).strip() if value else ''
+  values = [urlencode(value)]
+  if refile.match(value):
+    parsed = mwp.parse(value)
+    values = [urlencode(wikilink.title.strip()) for wikilink in parsed.ifilter_wikilinks()]
+  return [f'{logoBase}/{v}' for v in values if v.endswith('.svg')]
+
 # See https://www.mediawiki.org/wiki/Manual:Page_title#Encoding
 allowed = frozenset(b'\'(),-.0123456789:ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz')
 def urlencode(value):
-  chars = [];
+  chars = []
   for b in value.encode():
     if b in allowed:
       chars.append(chr(b))
@@ -103,22 +157,21 @@ def urlencode(value):
       chars.append("%%%0.2X" % b)
   return ''.join(chars)
 
-refile = re.compile(r'(?i)^ *\[\[ *file *:')
-def logoValueToUrls(value):
-  value = str(value).strip() if value else ''
-  values = [urlencode(value)]
-  if refile.match(value):
-    parsed = mwp.parse(value)
-    values = [urlencode(wikilink.title.strip()) for wikilink in parsed.ifilter_wikilinks()]
-  return [f'{logoBase}/{v}' for v in values if v.endswith('.svg')]
-
 def main(url):
-  markupParser = MarkupHandler()
+  global startTime
+  startTime = datetime.utcnow()
+  output = JsonHandler()
+  markupParser = MarkupHandler(output)
   xmlParser = TextHandler(lambda base, title, text : markupParser.process(base, title, text))
   with urllib.request.urlopen(url) as byteStream:
     decompress(byteStream, lambda data : xmlParser.feed(data))
-  print('articleCount', articleCount)
-  print('infoCount', infoCount)
-  print('svgLogoCount', svgLogoCount)
+  output.end()
+  now = datetime.utcnow()
+  delta = now - startTime
+  print('articleCount', articleCount, file=sys.stderr)
+  print('infoCount', infoCount, file=sys.stderr)
+  print('svgLogoCount', svgLogoCount, file=sys.stderr)
+  print('elapsed', delta, file=sys.stderr)
 
-main(url)
+if __name__ == '__main__':
+  main(url)
